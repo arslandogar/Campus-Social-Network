90fbbe48b877f33813a04322b0c6e44b5968fbb7
arslandogar <arslandogar112@yahoo.com>
2018-10-24 18:09:28 +0500

    Front-end started

---
+++ CampusSocialNetwork/CampusSocialNetwork/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CampusSocialNetwork.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddClass()
+        {
+            return View();
+        }
+        public ActionResult AddTeacher()
+        {
+            return View();
+        }
+
+    }
+}
